12d3cfeda735a893e50368050395de2d969504da
laing raven <laingraven.java@gmail.com>
2021-06-04 19:19:39 +0700

    add part 2

---
+++ part 2/chart-application/src/com/raven/component/Item_People.java
@@ -0,0 +1,52 @@
+package com.raven.component;
+
+public class Item_People extends javax.swing.JPanel {
+
+    public Item_People(String name) {
+        initComponents();
+        lb.setText(name);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        imageAvatar1 = new com.raven.swing.ImageAvatar();
+        lb = new javax.swing.JLabel();
+
+        setBackground(new java.awt.Color(229, 229, 229));
+
+        imageAvatar1.setBorderSize(0);
+        imageAvatar1.setImage(new javax.swing.ImageIcon(getClass().getResource("/com/raven/icon/profile.png"))); // NOI18N
+
+        lb.setFont(new java.awt.Font("sansserif", 0, 14)); // NOI18N
+        lb.setText("Name");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(imageAvatar1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lb, javax.swing.GroupLayout.DEFAULT_SIZE, 138, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(imageAvatar1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lb, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private com.raven.swing.ImageAvatar imageAvatar1;
+    private javax.swing.JLabel lb;
+    // End of variables declaration//GEN-END:variables
+}
