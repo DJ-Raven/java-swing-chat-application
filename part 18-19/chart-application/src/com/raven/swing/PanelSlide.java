04f1e7965de6a2c374b590faf3f7a624393cd464
laing raven <laingraven.java@gmail.com>
2021-10-04 21:14:16 +0700

    fix panel slider

--- part 18-19/chart-application/src/com/raven/swing/PanelSlide.java
+++ part 18-19/chart-application/src/com/raven/swing/PanelSlide.java
@@ -18,7 +18,6 @@ public class PanelSlide extends javax.swing.JPanel {
     }
 
     public PanelSlide() {
-        initComponents();
         list = new ArrayList<>();
         timer = new Timer(0, new ActionListener() {
             @Override
@@ -92,23 +91,4 @@ public class PanelSlide extends javax.swing.JPanel {
             }
         }
     }
-
-    @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 336, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 283, Short.MAX_VALUE)
-        );
-    }// </editor-fold>//GEN-END:initComponents
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    // End of variables declaration//GEN-END:variables
 }
